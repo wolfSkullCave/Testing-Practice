5067ac9d29f275fb256a4c24e80d248287b20934
Kyle-Laptop <67805222+wolfSkullCave@users.noreply.github.com>
2026-05-09 17:01:01 +0200

    feat(index.js): Added a calculator object, reverseString and capitilize functions.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-import "./styles.css";
-
 console.log("Hello Webpack Template!");
+
+export function capitalize(words) {
+  const cap = words.charAt(0).toUpperCase();
+  words = cap + words.substr(1);
+  return words;
+}
+
+export function reverseString(word) {
+  const wordArr = word.split("");
+  return wordArr.reverse().toString().replaceAll(",", "");
+}
+
+export const calculator = {
+  add(num1, num2) {
+    return num1 + num2;
+  },
+  subtr(num1, num2) {
+    return num1 - num2;
+  },
+  mult(num1, num2) {
+    return num1 * num2;
+  },
+  div(num1, num2) {
+    return num1 / num2;
+  },
+};
---
+++ src/index.test.js
@@ -0,0 +1,22 @@
+import { capitalize, reverseString, calculator } from ".";
+
+test("Capitalizes the first character in a string", () => {
+  expect(capitalize("hello")).toBe("Hello");
+  expect(capitalize("world")).toBe("World");
+});
+
+test("Take a string and reverse it", () => {
+  expect(reverseString("hello")).toBe("olleh");
+  expect(reverseString("world")).toBe("dlrow");
+});
+
+test("Perform basic addition, subtraction, multiplication and division with 2 numbers", () => {
+  expect(calculator.add(10, 2)).toBe(12);
+  expect(calculator.add(20, 4)).toBe(24);
+  expect(calculator.subtr(10, 5)).toBe(5);
+  expect(calculator.subtr(20, 5)).toBe(15);
+  expect(calculator.mult(20, 5)).toBe(100);
+  expect(calculator.mult(16, 20)).toBe(320);
+  expect(calculator.div(10, 2)).toBe(5);
+  expect(calculator.div(200, 16)).toBe(12.5);
+});
